cb213a0b2e1b5d2d52cd0f6309ae9ca701c6504b
elston.shay <elston.shay@awatapu.school.nz>
2021-09-13 13:02:50 +1200

    Num check

---
+++ Instructions_v1.py
@@ -0,0 +1,36 @@
+
+
+# functions go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("please answer yes / no")
+
+
+
+def instructions():
+    print("**** How to play ****")
+    print()
+    print("The rules of the game go here")
+    print()
+    return ""
+
+
+# Main Routine goes here...
+show_instructions = yes_no ("have you played the game before? ")
+
+if show_instructions == "no":
+    instructions()
+
+print("Program continues")
